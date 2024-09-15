593fe2cfc5d89820299ba9eef1a9a7582f4e3be9
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-09-15 16:49:02 +0600

    parsing data

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -4,35 +4,46 @@ import 'package:get_it/get_it.dart';
 import 'package:messenger/services/database_service.dart';
 import 'package:messenger/services/navigation.dart';
 
+import '../models/chat_user.dart';
 
-
-class AuthnticationProvider extends ChangeNotifier{
+class AuthnticationProvider extends ChangeNotifier {
   late final FirebaseAuth _auth;
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
+  late ChatUser user;
 
-  AuthenticationProvider(){
+  AuthenticationProvider() {
     _auth = FirebaseAuth.instance;
     _navigationService = GetIt.instance.get<NavigationService>();
     _databaseService = GetIt.instance.get<DatabaseService>();
 
     _auth.authStateChanges().listen((_user) {
       if (_user != null) {
-_databaseService.updateUserLastSeentime(_user.uid);
-_databaseService.getUser(_user.uid);
-      } else {
-
-      }
+        _databaseService.updateUserLastSeentime(_user.uid);
+        _databaseService.getUser(_user.uid).then(
+          (_snapshot) {
+            Map<String, dynamic> _userData =
+                _snapshot.data()! as Map<String, dynamic>;
+            user = ChatUser.fromJson({
+                "uid": _user.uid,
+              "name": _userData["name"],
+              "last_active": _userData["last_active"],
+              "image": _userData["image"],
+            });
+          },
+        );
+      } else {}
     });
   }
-  Future<void> loginUsingEmailAndPassword(String _email, String _password) async {
-    try{
+
+  Future<void> loginUsingEmailAndPassword(
+      String _email, String _password) async {
+    try {
       await _auth.signInWithEmailAndPassword(
-          email: _email,
-          password: _password);
-    }on FirebaseAuthException{
+          email: _email, password: _password);
+    } on FirebaseAuthException {
       print('Error login into firebase');
-    }catch (e){
+    } catch (e) {
       print(e);
     }
   }
